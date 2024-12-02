fff21a0240726e7ef8c487c127b92b7e61846442
Stk666666r <rodrigezcalerofranciscojavier@gmail.com>
2024-12-02 11:08:20 +0100

    CommitFran5

--- Ej3/src/model/VerificacionFecha.java
+++ Ej3/src/model/VerificacionFecha.java
@@ -1,11 +1,26 @@
 package model;
 import java.time.LocalDate;
+import java.util.Calendar;
 
 public interface VerificacionFecha {
 	
 	  
-	   
-   boolean VerificacionFecha(int meses, int anios);
-	
+	 int DIA_DEL_MES = Calendar.DAY_OF_MONTH;
+	    int MES = Calendar.MONTH;
+	    int ANO = Calendar.YEAR;
 
-}
+	    /**
+	     * Verifica si se cumple un mes entre la fecha de apertura y la fecha del sistema.
+	     * 
+	     * @return true si se cumple un mes, false en caso contrario.
+	     */
+	    boolean seCumpleMes();
+
+	    /**
+	     * Verifica si se cumple un año entre la fecha de apertura y la fecha del sistema.
+	     * 
+	     * @return true si se cumple un año, false en caso contrario.
+	     */
+	    boolean seCumpleAno();
+	}
+
